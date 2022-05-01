7135dee90ae592977fd3983b65175b9ae45b5db9
Fevzi Yüksel <93267352+FevziYuksel@users.noreply.github.com>
2022-05-02 00:21:31 +0300

    Fix tuple , set and dict functionality

--- Python Temel/Flatten1_patika.py
+++ Python Temel/Flatten1_patika.py
@@ -1,10 +1,11 @@
-inp = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+inp1 = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+inp = [(15,7),{"a":(3,2)}, [7,(3,4)]]
 out = []
 
 def flatten(inp):
     
     for i in inp:
-        if type(i) == list :
+        if type(i) == tuple or type(i) == list or type(i) == set or type(i) == dict :
             flatten(i)
         else:
             out.append(i)
