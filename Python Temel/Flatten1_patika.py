cd301452597e356388735c162dc02405d4ec3575
Fevzi Yüksel <93267352+FevziYuksel@users.noreply.github.com>
2022-05-02 03:24:10 +0300

    Update value-key issue in dictionaries

--- Python Temel/Flatten1_patika.py
+++ Python Temel/Flatten1_patika.py
@@ -2,6 +2,7 @@ inp1 = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 inp = [(15,7),{"a":(3,2)}, [7,(3,4)]]
 out = []
 
+#Flatten dictionaries by their keys.
 def flatten(inp):
     
     for i in inp:
@@ -13,5 +14,20 @@ def flatten(inp):
 
 print(flatten(inp))
         
+out = []
 
+#Flatten dictionaries by their values.
+def flatten(inp):
     
+    if type(inp) == dict:
+        inp = inp.values()
+        
+    for i in inp:
+        if type(i) == tuple or type(i) == list or type(i) == set or type(i) == dict:
+            flatten(i)
+        else:
+            out.append(i)
+    return out
+    
+print(flatten(inp))   
+
